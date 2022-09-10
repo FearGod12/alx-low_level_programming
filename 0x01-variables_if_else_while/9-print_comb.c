30a1ae86269cfdb6f23e04c0ac0f4ffc41fb7602
onyenike chukwudi FearGod <onyenikechukwudi@gmail.com>
2022-09-10 13:47:16 +0000

    print numbers with comma and space

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,15 +9,19 @@
 * Description: Prints the numbers of base 10
 * only putchar is allowed
 * Return: 0 for success
-*/int main(void)
+*/
+int main(void)
 {
 	int num = 48;
 
 	while (num <= 57)
 	{
 		putchar(num);
-		putchar(',');
-		putchar(' ');
+		if (num < 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 		num++;
 	}
 	putchar('\n');
