c3d15aadcbb9cfb1627f5da8a0e04b12f8fb030e
onyenike chukwudi FearGod <onyenikechukwudi@gmail.com>
2022-09-11 19:39:45 +0000

    how to use write to print to the screen

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
 
 /**
- *  * main - Prints quote to the standard error
- *   *
- *    * Return: zero on success
- *     *
+* main - Prints quote to the standard error
+* description: a program to use write function to print to screen
+*
+* Return: 1 on success
 */
 int main(void)
-
-{	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"	58);
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"	,60);
 	return (1);
 }
