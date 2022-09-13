d61a91addc9fb32d2e57733d8fa15012a3e87998
onyenike chukwudi FearGod <onyenikechukwudi@gmail.com>
2022-09-13 14:48:20 +0000

    prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main- prints _putchar
+*
+* return: returns 0 for success
+*/
+int main()
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
