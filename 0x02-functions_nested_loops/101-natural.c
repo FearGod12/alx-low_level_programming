86a6f7f1ad4f4326c89f1f80d976666e191d6a3a
onyenike chukwudi FearGod <onyenikechukwudi@gmail.com>
2022-09-13 22:12:11 +0000

    multiples of 3 and 5 in 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int i;
+	int sum = 0;
+	for (i = 1; i < 1024; i++)
+	{
+		if((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum = sum + i;
+		}
+	}
+	printf("%d \n", sum);
+}
+
