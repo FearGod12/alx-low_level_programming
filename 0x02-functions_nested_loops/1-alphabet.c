770400de26df704db26299afb124f4bc5224d3f1
onyenike chukwudi FearGod <onyenikechukwudi@gmail.com>
2022-09-13 16:51:30 +0000

    main.h file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,6 @@
-#include <unistd.h>
-#include <stdio.h>
+#include "main.h"
 
-/**
-* description: a function that prints the lower case alphabets
+/* description: a function that prints the lower case alphabets
 * @void: returns no value
 */
 void print_alphabet(void)
@@ -11,11 +9,7 @@ void print_alphabet(void)
 
 	for (i = 'a'; i <= 'z'; i++)
 	{
-		write(1, &i, 1);
+		_putchar(i);
 	}
-	putchar('\n');
-}int main(void)
-{
-	print_alphabet();
-	return (0);
+	_putchar('\n');
 }
