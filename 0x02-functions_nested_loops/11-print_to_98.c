3fca9d1147ed9ee926c8cef1d9152965a1b04729
onyenike chukwudi FearGod <onyenikechukwudi@gmail.com>
2022-09-13 20:59:58 +0000

    numbers to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_to_98 - prints from number given to 98
+* @n: integer number passed by user
+* @void: has no return value
+*/
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d ", n);
+			putchar(',');
+			putchar(' ');
+			n--;
+		}
+	}
+	else if (n < 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			putchar(',');
+			putchar(' ');
+			n++;
+		}
+	}
+	else 
+		printf("%d", n);
+		putchar(',');
+		putchar(' ');
+}
