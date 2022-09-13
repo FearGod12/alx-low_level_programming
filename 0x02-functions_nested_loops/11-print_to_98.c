d85efd5b1f507c631c0c9cb2b947681a6c5d27a2
onyenike chukwudi FearGod <onyenikechukwudi@gmail.com>
2022-09-13 21:38:06 +0000

    numbers to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,31 +7,11 @@
 */
 void print_to_98(int n)
 {
-	if (n > 98)
-	{
-		while (n > 98)
-		{
-			printf("%d,", n);
-			putchar(' ');
-			if (n == 98)
-				printf("98");
-			n--;
-		}
-	}
-	else if (n < 98)
-	{
-		while (n < 98)
-		{
-			printf("%d,", n);
-			putchar(' ');
-			if (n == 98)
-				printf("98");
-			n++;
-		}
-	}
+	if (n <= 98)
+		for (n = n; n <= 97; n++)
+			printf("%d, ", n);
 	else
-	{
-		printf("%d", n);
-		putchar(' ');
-	}
+		for (n = n; n > 98; n--)
+			printf("%d, ", n);
+	printf("98\n");
 }
