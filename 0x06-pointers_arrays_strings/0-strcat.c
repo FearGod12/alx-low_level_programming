75df72e178ec1a9b9ccf72c41b713e07ed45c67d
FearGod12 <onyenikechukwudi@gmail.com>
2022-09-21 06:10:26 -0700

    concatenates strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,9 +8,10 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int i;
+	int j = 0;
 
-	for (j = 0; dest[j] < '\0'; dest++)
+	while (dest[j] != '\0')
 		j++;
 	for (i = 0; i < j && src[i] != '\0'; i++)
 		dest[j + 1] = src[i];
