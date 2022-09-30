7ac344f69aa022cd216f5c6584ca4098c443b251
FearGod12 <onyenikechukwudi@gmail.com>
2022-09-30 04:19:06 -0700

    prints program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the program name
+ * @argc: number  of arguments
+ * @argv: array of arguments
+ * Return: always 0 dor success
+ */
+int main(int argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+	argc = argc;
+	return (0);
+}
