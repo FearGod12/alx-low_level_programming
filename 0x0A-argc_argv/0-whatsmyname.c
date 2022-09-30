44141d591319fe96546e9a8f93c6f2af4b66af60
FearGod12 <onyenikechukwudi@gmail.com>
2022-09-30 07:41:39 -0700

    prints program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: array of arguments
  * Return: always 0 dor success
  */
-int main(int __attribute__ ((unused))argc, char* argv[])
+int main(int argc __attribute__ ((unused)), char* argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
