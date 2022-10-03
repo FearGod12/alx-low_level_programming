c8acaf01237c03828c8245f25074a5d350ea7b3b
FearGod12 <onyenikechukwudi@gmail.com>
2022-10-03 09:33:46 -0700

    creates an array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	t =  (char *) malloc(sizeof(int) * size);
+	t =  (char *) malloc(sizeof(char) * size);
 	if (t == NULL)
 		return (t);
 	while (i < size)
