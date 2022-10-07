7e842142143e4ce8b276a88b67968778530264dc
FearGod12 <onyenikechukwudi@gmail.com>
2022-10-07 02:25:40 -0700

    allocates memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory
+ * @b: unsigned integer whose size is to be allocated
+ * description: if malloc fails the process terminates with
+ * a status value of 98
+ * Return: a pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(sizeof(int));
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
