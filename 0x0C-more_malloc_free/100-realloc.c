b473593c821205cf46a848877d6c7fbfe8ac65b7
FearGod12 <onyenikechukwudi@gmail.com>
2022-10-07 09:05:33 -0700

    implementation of realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,19 +1,6 @@
 #include "main.h"
 
 /**
- * lenght - finds the lenght of a string
- * @ptr: pointer to the string
- * Return: lenght of the string in integer
- */
-unsigned  int lenght(char *ptr)
-{
-	unsigned int  i, len = 1;
-
-	for (i = 0; ptr[i] != '\0'; i++)
-		len++;
-	return (len);
-}
-/**
  * _realloc - reallocates memory
  * @ptr: pointer to old memory location
  * @old_size: the old size of memory
@@ -23,42 +10,21 @@ unsigned  int lenght(char *ptr)
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *pptr, *k;
-	unsigned int i;
-
-	k = ptr;
 	if (new_size == old_size)
 		return (ptr);
 	if (ptr == NULL)
 	{
-		free(ptr);
-		pptr = malloc(new_size);
-		if (pptr == NULL)
+		ptr = malloc(new_size);
+		if (ptr == NULL)
 			return (NULL);
-		return (pptr);
+		return (ptr);
 	}
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	if (new_size > old_size)
-	{
-		pptr = malloc(new_size);
-		if (pptr == NULL)
-			return (NULL);
-		for (i = 0; i < lenght(ptr); i++)
-			pptr[i] = k[i];
-		free(ptr);
-	}
-	if (new_size < old_size)
-	{
-		pptr = malloc(new_size);
-		if (pptr == NULL)
-			return (NULL);
-		for (i = 0; i < new_size; i++)
-			pptr[i] = k[i];
-		free(ptr);
-	}
-	return (pptr);
+	free(ptr);
+	ptr = malloc(new_size);
+	return (ptr);
 }
