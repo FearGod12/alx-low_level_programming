077751fe21afb3eed0e8595db35706de75bf75f1
FearGod12 <onyenikechukwudi@gmail.com>
2022-10-10 05:11:55 -0700

    create a struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * struct dog - contains information about a dog
+ * @name: name of the dog
+ * @age: age of thw dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,8 @@
+#include <stddef.h>
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+int _putchar(char c);
+
+#endif
