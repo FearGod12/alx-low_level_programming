567045ab32d90dd09941e35433d0f9156f32695c
FearGod12 <onyenikechukwudi@gmail.com>
2022-10-13 11:45:29 -0700

    prints numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,6 +14,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d%s", va_arg(ap, int), separator);
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
 	}
 	va_end(ap);
 	printf("\n");
