efa74d020dfa0424649a703f113448df8cc2876f
FearGod12 <onyenikechukwudi@gmail.com>
2022-10-24 22:09:27 +0000

    prints lists safe

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,13 +9,18 @@ size_t print_listint_safe(const listint_t *head)
 {
 	size_t i = 1;
 
-	if (!head)
+	if (head == NULL)
 		exit(98);
-	while (temp)
+	while (head != NULL)
 	{
 		i++;
-		printf("%d\n", temp->n);
-		temp = temp->next;
+		printf("[%p] %d\n", (void *)head, head->n);
+		if (head->next >= head)
+		{
+			printf("-> [%p] %d\n", (void *)head->next, (head->next)->n);
+			break;
+		}
+		head = head->next;
 	}
 	return (i);
 }
