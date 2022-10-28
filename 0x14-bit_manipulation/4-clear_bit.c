2c305eec20612fcdfee8af586838c533c4c6d0ef
FearGod12 <onyenikechukwudi@gmail.com>
2022-10-28 18:16:54 +0000

    modified file

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * claer_bit - set the value of a bit to 0 at a given index.
+ * clear_bit - set the value of a bit to 0 at a given index.
  * @n: pointer to the number
  * @index: is the index, starting from 0 of the bit you want to set
  * Return:  1 if it worked, or -1 if an error occurred
