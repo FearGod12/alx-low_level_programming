329c7837f0e035a5852fee869cb2e36d0891dd3b
FearGod12 <onyenikechukwudi@gmail.com>
2022-11-01 14:34:18 +0000

    updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,6 +7,7 @@
 int _strlen(char *s)
 {
 	int i;
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		;
@@ -24,6 +25,7 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	ssize_t bytes_written;
 	unsigned int lenght;
+
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
