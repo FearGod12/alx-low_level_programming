53e40fecbee6afd86ad2a99d81b80914c44a60fc
FearGod12 <onyenikechukwudi@gmail.com>
2022-11-01 14:24:25 +0000

    updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,15 +22,21 @@ int _strlen(char *s)
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	size_t bytes_written;
-	int lenght;
-	if (!filename)
+	ssize_t bytes_written;
+	unsigned int lenght;
+	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT | O_EXCL, 600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	lenght = _strlen(text_content);
-	bytes_written = write(filename, text_content, lenght)
+	if (text_content == NULL)
+	{
+		lenght = 0;
+		text_content = "";
+	}
+	else
+		lenght = _strlen(text_content);
+	bytes_written = write(fd, text_content, lenght);
 	if (bytes_written == -1)
 		return (-1);
 	close(fd);
