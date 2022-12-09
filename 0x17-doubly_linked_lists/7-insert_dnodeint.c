c848a3e0dce90f64ceb6b657bf3d7dc122a2a388
FearGod12 <onyenikechukwudi@gmail.com>
2022-12-09 10:25:21 +0000

    [fix] files 7: insertion

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,12 +26,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		free(new);
-		return (add_dnodeint(&*h, n));
+		return (add_dnodeint(h, n));
 	}
 	else if (idx == len + 1)
 	{
 		free(new);
-		return (add_dnodeint_end(&*h, n));
+		return (add_dnodeint_end(h, n));
 	}
 	else
 	{
