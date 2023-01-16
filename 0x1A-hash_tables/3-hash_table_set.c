e8323259e4ce7b0b76a69d7399c0b5981055fcd1
FearGod12 <onyenikechukwudi@gmail.com>
2023-01-16 14:10:21 +0000

    task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,41 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ *
+ * @ht: pointer to the hash table
+ * @key: key associated with the item can not be an empty string
+ * @value: value associated with the item value must be duplicated
+ * value can be an empty string
+ * Return:1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *element, *array;
+	unsigned long int index;
+
+	if (!key || strlen(key) == 0 || !ht || !value)
+		return (0);
+	element = malloc(sizeof(hash_node_t));
+	if (!element)
+		return (0);
+	element->key = strdup(key);
+	element->value = strdup(value);
+	index = key_index((const unsigned char *) key, ht->size);
+	array = ht->array[index];
+	if (!array)
+	{
+		element->next = NULL;
+		array = element;
+	}
+	else if (strcmp(array->key, key) == 0)
+	{
+		array->value = strdup(value);
+	}
+	else
+	{
+		element->next = array;
+		array = element;
+	}
+	return (1);
+}
